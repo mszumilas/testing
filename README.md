57a03d1d920c683200ae169b659d35b7dfa9705a
mszumilas <szumi321@gmail.com>
2026-06-19 01:32:55 +0200

    add multiple runs and readme, update gitignore

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# testing
+# Testy E2E Playwright – ING.pl (Cookies)
+
+## 📌 Opis projektu
+
+Projekt zawiera automatyczne testy E2E dla strony **ING.pl**, napisane w Python + Playwright.  
+Głównym celem jest:
+
+- obsługa bannera cookies ING,
+- akceptacja tylko wymaganych cookies,
+- walidacja, że żadne cookies marketingowe nie zostały ustawione,
+- uruchamianie testów na Chrome i Firefox.
+
+
+---
+
+## 🧰 Wymagania
+
+- Python 3.9+
+- Playwright 1.40+
+- pytest
+- pip
+
+---
+
+## 📦 Instalacja
+
+```bash
+pip install pytest pytest-playwright
+playwright install
+```
+
+---
+
+## 🌐 Uruchamianie testów na Chrome i Firefox
+
+```bash
+pytest
+```
+W celu zmiany przegladarek należy zmienić konfiguracje w pliku conftest.py linijka 22 i wpisać w parametry żądane przeglądarki. 
+
+W celu uruchomienia testów bez trybu headless należy zmienić w pliku conftest.py w linijce 28 (headless=True) na (headless=False).
--- conftest.py
+++ conftest.py
@@ -1,14 +1,31 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="append",
+        default=["chromium"],
+        help="Browsers to run tests against: chromium, firefox, webkit",
+    )
 
+@pytest.fixture(scope="session")
+def playwright_instance():
+    with sync_playwright() as p:
+        yield p
+
+@pytest.fixture(scope="session")
+def browser_type(request, playwright_instance):
+    browsers = request.config.getoption("--browser")
+    return [getattr(playwright_instance, b) for b in browsers]
+
+@pytest.fixture(params=["chromium", "firefox"])
+def browser(request, playwright_instance):
+    return getattr(playwright_instance, request.param)
 
 @pytest.fixture
-def page():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context()
-        context.clear_cookies()
-        page = context.new_page()
-        yield page
-        browser.close()
+def page(browser):
+    context = browser.launch(headless=True).new_context()
+    page = context.new_page()
+    yield page
+    context.close()
