23b960a6fa5b9b34ba86fdf3486ce2da2db3956d
mszumilas <szumi321@gmail.com>
2026-06-19 02:00:07 +0200

    Update cookies_test.py
    add analytical cookies accept and assertion

--- tests/cookies_test.py
+++ tests/cookies_test.py
@@ -6,11 +6,24 @@ def test_customize_and_accept_selected(page):
     cookies = CookiesBanner(page)
 
     cookies.open_customization()
-
+    cookies.enable_analytical()
     cookies.accept_selected()
-
+    page.reload()
     cookies = page.context.cookies()
+    analytical_prefixes = [
+        "_ga",
+        "_gid",
+        "_gat"
+    ]
 
+    found = {prefix: False for prefix in analytical_prefixes}
+
+    for cookie in cookies:
+        for prefix in analytical_prefixes:
+            if cookie["name"].startswith(prefix):
+                found[prefix] = True
+    assert any(found.values()), f"Brak cookies analitycznych! Znaleziono: {cookies}"
+    
     forbidden_prefixes = [
         "_fbp",
         "_gcl",
